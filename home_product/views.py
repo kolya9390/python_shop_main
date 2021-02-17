30f23c551c1608889070d035d6148f37937055b8
kolya <kolya9390@gmail.com>
2021-02-17 20:36:24 +0700

    Init

--- home_product/views.py
+++ home_product/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+
+def home(request):
+    products = Product.objects
+    return render(request, 'home_product/home.html', {'products': products})
